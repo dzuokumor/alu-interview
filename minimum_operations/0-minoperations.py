dc4162fe7f772fe4f4907c50073df7c4a5dc0605
dzuokumor <d.zuokumor@alustudent.com>
2024-09-29 10:53:48 -0700

    Minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Minimum Operations"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    i = 2
+    result = 0
+    while i <= n:
+        if n % i == 0:
+            result += i
+            n = n / i
+        else:
+            i += 1
+    return result
